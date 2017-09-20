8ed16eb067ee6481e63b88bebbc1764e1f6752a5
Ramon Simó <jrsimo>
2017-09-20 20:49:16 +0200

    Práctica 1 Repaso: Solución subida

---
+++ Clase1909-Practica1/src/com/jrsimo/di/Formato.java
@@ -0,0 +1,5 @@
+package com.jrsimo.di;
+
+public enum Formato {
+    WAV,MP3,MIDI,AVI,MOV,MPG,CDAUDIO,DVD
+}
---
+++ Clase1909-Practica1/src/com/jrsimo/di/Genero.java
@@ -0,0 +1,5 @@
+package com.jrsimo.di;
+
+public enum Genero {
+    ROCK, POP, SOUL, FUNKY, FLAMENCO, CLASICA, BLUES, TECNO, OPERA, PUNK, JAZZ, HIPHOP
+};
